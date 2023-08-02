41acb51639df112bf2f46755b4c036fd5dfd93a0
mgmetehan <mgmetehan@hotmail.com>
2023-08-02 22:39:40 +0300

    init project

---
+++ src/main/java/com/mgmetehan/SocialPostHub/SocialPostHubApplication.java
@@ -0,0 +1,13 @@
+package com.mgmetehan.SocialPostHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPostHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPostHubApplication.class, args);
+	}
+
+}
